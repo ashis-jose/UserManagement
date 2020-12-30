3137f543c0ccd3d5c5cfdf72fbbd3f64b8ac13f9
ashis <ashisjose@gmail.com>
2020-12-30 20:07:18 +0530

    dummy search included

--- UserManagementApp/src/app/user-details/user-details.component.ts
+++ UserManagementApp/src/app/user-details/user-details.component.ts
@@ -17,6 +17,12 @@ export class UserDetailsComponent implements OnInit {
   private toastr:ToastrService) { }
   public showModal : boolean = false;
   public searchInput : string;
+  public searchInput1 : string;
+  public searchInput2 : string;
+  public searchInput3 : string;
+  public searchInput4 : string;
+  public searchInput5 : string;
+  public searchInput6 : string;
   
  
   inputKey : string;
@@ -46,23 +52,70 @@ export class UserDetailsComponent implements OnInit {
     }
   }
   
-  search(){
-    
-    
+  searchFN(){
     if (this.searchInput!=""){
-      
-      
       this.service.list = this.service.list.filter(res => {
         return res.firstName.toLocaleLowerCase().includes(this.searchInput.toLocaleLowerCase());
       });
-      
-
-    
-    }else if(this.searchInput == "" ){
+      }else if(this.searchInput == "" ){
       this.service.refreshList();
     }
-    
   }
+  searchLN(){
+    if (this.searchInput1!=""){
+      this.service.list = this.service.list.filter(res => {
+        return res.lastName.toLocaleLowerCase().includes(this.searchInput1.toLocaleLowerCase());
+      });
+      }else if(this.searchInput1 == "" ){
+      this.service.refreshList();
+    }
+  }
+  searchEM(){
+    if (this.searchInput2!=""){
+      this.service.list = this.service.list.filter(res => {
+        return res.email.toLocaleLowerCase().includes(this.searchInput2.toLocaleLowerCase());
+      });
+      }else if(this.searchInput2 == "" ){
+      this.service.refreshList();
+    }
+  }
+  searchP(){
+    if (this.searchInput3!=""){
+      this.service.list = this.service.list.filter(res => {
+        return res.phone.toLocaleLowerCase().includes(this.searchInput3.toLocaleLowerCase());
+      });
+      }else if(this.searchInput3 == "" ){
+      this.service.refreshList();
+    }
+  }
+  searchAD(){
+    if (this.searchInput4!=""){
+      this.service.list = this.service.list.filter(res => {
+        return res.address.toLocaleLowerCase().includes(this.searchInput4.toLocaleLowerCase());
+      });
+      }else if(this.searchInput4 == "" ){
+      this.service.refreshList();
+    }
+  }
+  searchG(){
+    if (this.searchInput5!=""){
+      this.service.list = this.service.list.filter(res => {
+        return res.gender.toLocaleLowerCase().includes(this.searchInput5.toLocaleLowerCase());
+      });
+      }else if(this.searchInput5 == "" ){
+      this.service.refreshList();
+    }
+  }
+  searchAC(){
+    if (this.searchInput6!=""){
+      this.service.list = this.service.list.filter(res => {
+        return res.active.toLocaleLowerCase().includes(this.searchInput6.toLocaleLowerCase());
+      });
+      }else if(this.searchInput6 == "" ){
+      this.service.refreshList();
+    }
+  }
+
   
 
 }
